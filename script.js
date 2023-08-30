11db599a61336811b4fdbf653a6ff4342ee44b0c
mikeycb96 <michaelsonbaptist@gmail.com>
2023-08-30 19:02:42 +0100

    mvp completed

--- script.js
+++ script.js
@@ -28,7 +28,38 @@ function populateCountryList() {
         li.textContent = `${country.name.common} - Population ${country.population}`;
         ul.appendChild(li);
     });
+
+
 }
+
+
+function handleFormSubmission(event) {
+    event.preventDefault(); 
+    const inputElement = document.getElementById('input-text');
+    const inputValue = inputElement.value;
+    filterAndUpdateList(inputValue);
+}
+
+
+function filterAndUpdateList(query) {
+    const filteredCountries = globalOutput.filter(country => {
+        const countryName = country.name.common.toLowerCase();
+        return countryName.includes(query.toLowerCase());
+    });
+
+    const ul = document.querySelector("ul");
+    ul.innerHTML = "";
+
+    filteredCountries.forEach(country => {
+        const li = document.createElement("li");
+        li.textContent = `${country.name.common} - Population: ${country.population}`;
+        ul.appendChild(li);
+    });
+}
+
+
+const form = document.getElementById('form');
+form.addEventListener('submit', handleFormSubmission);
 
 
 
